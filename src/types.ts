93a7c9d1552f171bb4e70a0b4d25b98b5672be33
Robert Sahakyan <r.sahakyan1@gmail.com>
2026-02-26 13:33:34 +0100

    add NocoDBContact and EspoCRMContact interfaces to types

--- src/types.ts
+++ src/types.ts
@@ -15,3 +15,60 @@ export type directoryResponse = {
     isLastPage: boolean;
   };
 };
+
+// Note: excludes Avatar for now
+export interface NocoDBContact {
+  Id: number;
+  CreatedAt: string;
+  UpdatedAt: string | null;
+  Surname: string;
+  FirstNames: string;
+  IsActiveMember: string;
+  Email: string;
+  Telephone: string | null;
+  MembershipAspirations: string | null;
+  ResidentLocality: string | null;
+  ResidentCountry: string | null;
+  Website: string | null;
+  Introduction: string | null;
+  Capacity: string | null;
+  CvUrl: string | null;
+  CV: string | null;
+  Provenance: string | null;
+  Connections: string | null;ß
+  Wants: string | null;
+  Offers: string | null;
+  Availability: string | null;
+  LastModifiedBy: string | null;
+  MatrixID: string | null;
+  JoinedAt: string | null;
+  OpenCollectiveId: string | null;
+  "Full Name": string | null;
+}
+
+/*
+Note: 
+These are only a subset of fields from our espoCRM "Contact" entity. 
+I took the relevant ones
+
+To-do: CV attachments
+*/
+export interface EspoCRMContact {
+  addressCity?: string;
+  addressCountry?: string;
+  cAvailability?: string;
+  cCvUrl?: string[];
+  cJoinedAt?: string;
+  cMatrixID?: string;
+  cPhoneNumber?: string;
+  cMembershipAspirations?: string;
+  cOpenCollectiveID?: string;
+  cWebsite?: string[];
+  cWorkCapacity?: string;
+  description?: string;
+  emailAddress?: string;
+  firstName?: string;
+  lastName?: string;
+  name?: string;
+  phoneNumber?: string;
+}
